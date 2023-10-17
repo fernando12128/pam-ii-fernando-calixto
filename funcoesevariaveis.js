7f140e68f3166c8c8ff79f172eb854a082cc2461
fernando12128 <fernando.augusto12128@gmail.com>
2023-10-16 22:26:13 -0300

    primeira aula de JavaScript

---
+++ funcoesevariaveis.js
@@ -0,0 +1,35 @@
+var variavel1 = "Ola"
+let variavel2 = 3
+const variavel3 = 8
+
+// console.log("Ola mundo!")
+// console.log(variavel1 + "Mundo")
+// console.log(variavel1, "mundo")
+
+// //para fazer contas
+// console.log(variavel2 + variavel3)//soma
+// console.log(variavel2 - variavel3)//subtração
+// console.log(variavel2 * variavel3)//multiplicação
+// console.log(variavel2 / variavel3)//divisão
+
+//para saber que tipo de varial está retornando /typeof
+console.log(typeof variavel1)
+
+//objeto
+const obj ={
+    nome:"jão",
+    signo:"Escopiãozão",
+    idade: 345
+}
+
+console.log(obj)
+
+console.log(
+    "Nome: ",obj.nome,
+    "\nIdade: ",obj.idade
+    // \n para quebrar linha
+)
+
+//array
+let cachorro=["bug","zeus","Osiris"]
+console.log(cachorro[2])
