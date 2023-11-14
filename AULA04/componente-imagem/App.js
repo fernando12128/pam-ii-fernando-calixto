52c254dcc99976db175c2d017aafca40412bacdb
fernando12128 <fernando.augusto12128@gmail.com>
2023-11-13 22:27:19 -0300

    Aula 04 apredendo documentação

---
+++ AULA04/componente-imagem/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {View, Text, Image, Switch, ScrollView, TextInput, Button} from 'react-native';
+
+const Pokemon = () => {
+  return (
+    <ScrollView>
+      <View>
+        <Text
+          style={{
+            fontSize:45,
+            alignSelf: "center",
+            color: "green",
+          }}
+        >
+          Snorlax Berrie
+          </Text>
+          <Image
+          source={{
+            uri: 'https://pbs.twimg.com/media/E4sYypaVgAQ1Cv9.png',
+          }}
+          style={{width:200, height:210, alignSelf: "center"}}
+          />
+
+        <Button
+        onPress={onPress}
+        title="AA"
+        />
+        <Switch
+          
+        />
+      </View>
+      <TextInput
+        style={{
+          height:40,
+          borderColor: 'gray',
+          borderWidth: 1,
+          padding: 5,
+          margin:15,
+        }}
+        defaultValue="Digite aquiii!"
+      />
+    </ScrollView>
+  );
+}
+
+export default Pokemon;
