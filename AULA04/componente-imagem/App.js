c668d11b7c398dc7f47a988f384195de52bb2a4e
Fernando Calixto <fernando12128>
2023-11-25 13:26:13 -0300

    atividade para segunda

--- AULA04/componente-imagem/App.js
+++ AULA04/componente-imagem/App.js
@@ -1,46 +1,57 @@
-import React from 'react';
-import {View, Text, Image, Switch, ScrollView, TextInput, Button} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image, Switch, ScrollView, TextInput } from 'react-native';
 
 const Pokemon = () => {
+  const [isSwitchOn, setIsSwitchOn] = useState(false);
+
+  const toggleSwitch = () => {
+    setIsSwitchOn((prev) => !prev);
+  };
+
+  const getImageSource = () => {
+    return isSwitchOn
+      ? { uri: 'https://m.media-amazon.com/images/I/712rNmu7YEL._AC_UF1000,1000_QL80_.jpg' }
+      : { uri: 'https://pbs.twimg.com/media/E4sYypaVgAQ1Cv9.png' };
+  };
+
   return (
     <ScrollView>
       <View>
         <Text
           style={{
-            fontSize:45,
-            alignSelf: "center",
-            color: "green",
+            fontSize: 45,
+            alignSelf: 'center',
+            color: 'green',
           }}
         >
           Snorlax Berrie
-          </Text>
-          <Image
-          source={{
-            uri: 'https://pbs.twimg.com/media/E4sYypaVgAQ1Cv9.png',
-          }}
-          style={{width:200, height:210, alignSelf: "center"}}
-          />
-
-        <Button
-        onPress={onPress}
-        title="AA"
+        </Text>
+        <Image
+          source={getImageSource()}
+          style={{ width: 200, height: 210, alignSelf: 'center' }}
         />
+
         <Switch
-          
+          trackColor={{ false: '#767577', true: '#81b0ff' }}
+          thumbColor={isSwitchOn ? '#f5dd4b' : '#f4f3f4'}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isSwitchOn}
+          style={{ marginTop: 10, alignSelf: 'center' }}
         />
       </View>
       <TextInput
         style={{
-          height:40,
+          height: 40,
           borderColor: 'gray',
           borderWidth: 1,
           padding: 5,
-          margin:15,
+          margin: 15,
         }}
-        defaultValue="Digite aquiii!"
+        defaultValue="Digite aqui!"
       />
     </ScrollView>
   );
-}
+};
 
 export default Pokemon;
